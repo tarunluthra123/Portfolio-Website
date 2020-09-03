b55d2a8ed369fc86927a645c4248d83bd8fba715
Tarun Luthra <tarunluthra987@gmail.com>
2020-09-03 12:57:15 +0530

    Updates

---
+++ precache-manifest.8da1d9b0808c5f23287bf6074957a29a.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f92337cac692f9af6c1c5a7c05b8ea0",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "722c153a91e904b926a9",
+    "url": "/Portfolio-Website/static/css/2.eb3a0a2e.chunk.css"
+  },
+  {
+    "revision": "2dac5a5fd55efe120048",
+    "url": "/Portfolio-Website/static/css/main.10ee23ba.chunk.css"
+  },
+  {
+    "revision": "722c153a91e904b926a9",
+    "url": "/Portfolio-Website/static/js/2.c9a8f8bf.chunk.js"
+  },
+  {
+    "revision": "0c361a26a3962dfc56455d709749d13c",
+    "url": "/Portfolio-Website/static/js/2.c9a8f8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dac5a5fd55efe120048",
+    "url": "/Portfolio-Website/static/js/main.f58e539f.chunk.js"
+  },
+  {
+    "revision": "b8c7e0cb4fb17c167de8",
+    "url": "/Portfolio-Website/static/js/runtime-main.5552b3a3.js"
+  },
+  {
+    "revision": "b6908c8488b972a0ae148f3ae64285a0",
+    "url": "/Portfolio-Website/static/media/MOS_PowerPoint.b6908c84.png"
+  },
+  {
+    "revision": "cbc513d50eac98c5edb4a0be559cb6de",
+    "url": "/Portfolio-Website/static/media/Resume.cbc513d5.pdf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Portfolio-Website/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Portfolio-Website/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Portfolio-Website/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Portfolio-Website/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Portfolio-Website/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "d508f80f98117261a42737d372777d6b",
+    "url": "/Portfolio-Website/static/media/facebook_card.d508f80f.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Portfolio-Website/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "7402c47b8d28ed56f9e96c7af3d67127",
+    "url": "/Portfolio-Website/static/media/github_profile_card.7402c47b.png"
+  },
+  {
+    "revision": "8ac3b5d4477a35d7c4e9f75fc077358d",
+    "url": "/Portfolio-Website/static/media/gmail_logo.8ac3b5d4.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Portfolio-Website/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Portfolio-Website/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Portfolio-Website/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Portfolio-Website/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Portfolio-Website/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "12bf03ff806a239d572d499cb3eec62e",
+    "url": "/Portfolio-Website/static/media/instagram_card.12bf03ff.png"
+  },
+  {
+    "revision": "b1c58ee19d1e2c38b29ae86f3174f2e4",
+    "url": "/Portfolio-Website/static/media/instagram_icon.b1c58ee1.svg"
+  },
+  {
+    "revision": "09a8a104e472c332d8d8581ed2327bc2",
+    "url": "/Portfolio-Website/static/media/linkedin_card.09a8a104.png"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/Portfolio-Website/static/media/linkedin_logo.4b475fb3.svg"
+  },
+  {
+    "revision": "51986d03a5d4fdef0f202a62c948386b",
+    "url": "/Portfolio-Website/static/media/maps_logo.51986d03.jpg"
+  },
+  {
+    "revision": "d287d24fa5b2b302d2e7e1bdf6f9734b",
+    "url": "/Portfolio-Website/static/media/me.d287d24f.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Portfolio-Website/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Portfolio-Website/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Portfolio-Website/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Portfolio-Website/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Portfolio-Website/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "7df18c72c85d42530ff19d81ca315aec",
+    "url": "/Portfolio-Website/static/media/sankalan_logo.7df18c72.jpg"
+  },
+  {
+    "revision": "5e19c2716e0b0e974cf239cfca0700c5",
+    "url": "/Portfolio-Website/static/media/sgtb_khalsa_logo.5e19c271.png"
+  },
+  {
+    "revision": "89aa396100676c79046a868b3f2de756",
+    "url": "/Portfolio-Website/static/media/umeed_logo.89aa3961.png"
+  }
+]);
